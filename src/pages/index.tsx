436b2888ab0465ba51d6d12ee787c6c6be4213d6
Minimalist-00 <kouki4122@icloud.com>
2023-10-13 19:51:28 +0900

    max-width 90%にしてみた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import TopContent from '@/components/TopContent'
 export default function Home() {
   return (
     <>
-      <main className="mx-auto max-w-[85%] text-[#333]">
+      <main className="mx-auto max-w-[90%] text-[#333]">
         <TopContent />
         <Gallery />
         <Footer />
